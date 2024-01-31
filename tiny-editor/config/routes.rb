4461fd770be0dfb0f9f695121d295ee086613c73
Andrei Sandler <setcursorpos@gmail.com>
2024-01-31 23:24:11 +0300

    Looks nicer

--- tiny-editor/config/routes.rb
+++ tiny-editor/config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root "main#index"
 
   get "/docs/:id", to: "docs#get"
-  post "/docs/:id", to: "docs#save"
   get "/docs/:id/edit", to: "docs#edit"
   post "/docs/:id/save", to: "docs#save"
   post "/docs/new", to: "docs#new"
