f1cf3f36c883cf027e9282a0190e17ea218be3a6
Andrei Sandler <setcursorpos@gmail.com>
2024-01-31 23:52:04 +0300

    Navigation

--- tiny-editor/app/controllers/docs_controller.rb
+++ tiny-editor/app/controllers/docs_controller.rb
@@ -1,23 +1,49 @@
 class DocsController < ApplicationController
     def get
-        @doc = Document.find(params[:id]) or not_found
+        @doc = get_doc(params[:id])
+    end
+
+    def all
+        @docs = Document.all.select("id", "name")
     end
 
     def edit
-        @doc = Document.find(params[:id]) or not_found
+        @doc = get_doc(params[:id])
     end
 
     def save
-        # validate params
-        d = Document.find(params[:id])
+        # validate params later
+        d = get_doc(params[:id])
         if d
             d.name = params[:doc_name]
             d.data = params[:doc_data]
-            d.save
-            redirect_to "/docs/#{params[:id]}"
-            return
+
+            if d.save
+                redirect_to "/docs/#{params[:id]}"
+                return
+            else
+                internal_error
+                return
+            end
         end
 
         redirect_to "/"
+    end
+
+    def new
+        d = Document.new
+        if d.save
+            redirect_to "/docs/#{d.id}/edit"
+        else
+            internal_error
+        end
+    end
+
+    def get_doc id
+        begin
+            return Document.find(params[:id])
+        rescue
+            not_found
+        end
     end
 end
--- tiny-editor/config/routes.rb
+++ tiny-editor/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root "main#index"
 
+  get "/docs/new", to: "docs#new"
+  get "/docs/all", to: "docs#all"
   get "/docs/:id", to: "docs#get"
   get "/docs/:id/edit", to: "docs#edit"
   post "/docs/:id/save", to: "docs#save"
-  post "/docs/new", to: "docs#new"
 end
