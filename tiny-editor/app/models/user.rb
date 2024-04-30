abda70bba90deef8a76344031b3cceff8be64fd8
Andrei Sandler <setcursorpos@gmail.com>
2024-04-30 11:05:11 +0300

    Move tiny-editor to different repo

--- tiny-editor/app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  authenticates_with_sorcery!
-
-  validates :email, uniqueness: true
-  validates :password, length: { minimum: 6 }
-
-  has_many :documents, dependent: :destroy
-  has_many :folders, dependent: :destroy
-end
