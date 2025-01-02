97c6ca97daa982d2e9c99d85215941e5ef546682
Andrei Sandler <setcursorpos@gmail.com>
2025-01-02 18:03:21 +0300

    Cleanup

--- book-editor/app/models/book.rb
+++
@@ -1,5 +0,0 @@
-class Book < ActiveRecord::Base
-    has_many :pages, dependent: :destroy
-    validates :title, presence: true
-    has_and_belongs_to_many :users
-end
--- book-editor/db/migrate/20150417182154_users_books_many_to_many.rb
+++
@@ -1,9 +0,0 @@
-class UsersBooksManyToMany < ActiveRecord::Migration[4.2]
-  def change
-    create_table :books_users, id: false do |t|
-      t.belongs_to :user, index: true
-      t.belongs_to :book, index: true
-      t.string :status
-    end
-  end
-end
